fed22a5c216e0022591b98313128ebce9f58afc8
hamedpro8 <hamedyaghoutpour0@gmail.com>
2021-03-11 18:19:56 +0330

    some edits

--- js.js
+++ js.js
@@ -1,39 +1,86 @@
+function newRecord(full_name){
+    var returnValue;
+    $.ajax({
+        url:"database.php",
+        method:"GET",
+        async:false,
+        data:{
+            action:"newRecord",
+            full_name:full_name
+        },
+        success:function(data){
+            if(data == "limitReached"){
+                returnValue = "limitReached"
+            }else{
+                returnValue = Number(data);
+            }
+            
+            
+        }
+
+    });
+    return returnValue;
+}
+function getAllRecordsAsJson(){ // return an array
+    var returnValue = false;
+    $.ajax({
+        url:"database.php",
+        method:"GET",
+        async:false,
+        data:{
+            action:"getAllRecordsAsJson"
+        },
+        success:function(data){
+            returnValue = JSON.parse(data)
+        }
+
+    })
+    return returnValue
+}
+function showNewItem(full_name) {
+    let returnValue = document.createElement("div")
+    returnValue.className = "item"
+    let svgIcon = document.createElement("img")
+    svgIcon.src="person.svg"
+    returnValue.appendChild(svgIcon)
+
+    let child2 = document.createElement("h1")
+    child2.innerHTML = "hamed"
+    returnValue.appendChild(child2)
+    return returnValue
+}
 $(document).ready(function(){
-    function updateRecords(){
-        $(".allRecords").empty();
-        getAllRecordsAsJson().forEach(element => {
-            $(".allRecords").append(
-                `
-                    <div class="item">
-                        <h1>${element}</h1>
-                    </div>
-                `
-            )
-        })
+function updateRecords(){
+    $(".allRecords").empty();
+    getAllRecordsAsJson().forEach(full_name => {
+        $(".allRecords").append(showNewItem(full_name))
+    })
+    $("#container .readedPagesCounter span").innerHTML = getAllRecordsAsJson().length
+}
+//updateRecords();
+
+$(".submitButton").click(function(){
+    let confirm = window.confirm("صحت اطلاعات ورودی را تایید می کنید؟")
+    if(! confirm){
+        return false;
+    };
+    var x = newRecord($("input").val())
+    if(x== "limitReached"){
+        alert(`
+            با عرض پوزش تمام صفحات این دوره از ختم قرآن تقسیم شده است، التماس دعا
+        `)
+    }else{
+        alert(`
+        سهم شما صفحه های:
+        ${x-4}
+        ${x-3}
+        ${x-2}
+        ${x-1}
+        ${x}
+
+        `)
     }
     updateRecords();
-    $(".submitButton").click(function(){
-        var x = newRecord($("input").val())
-        if(x== "limitReached"){
-            alert(`
-                با عرض پوزش تمام صفحات این دوره از ختم قرآن تقسیم شده است، التماس دعا
-            `)
-        }else{
-            alert(`
-            سهم شما صفحه های:
-            ${x-4}
-            ${x-3}
-            ${x-2}
-            ${x-1}
-            ${x}
-    
-            `)
-        }
-       
-    
-
-        
-        updateRecords();
-    })
+})
 
 })
